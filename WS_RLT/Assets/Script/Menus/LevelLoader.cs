18c112ed899c14c1c3b17e73736c4fb67f9c1daa
Lumiinaty <romain.nicoghossian@gmail.com>
2026-02-16 14:36:25 +0100

    TECH - Transitions Level marche, need to do tp transitions

---
+++ WS_RLT/Assets/Script/Menus/LevelLoader.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    
+    public Animator transition;
+    public float transitionTime = 1.0f;
+    
+
+    public void Transition()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        transition.SetTrigger("Start");
+        
+        yield return new WaitForSeconds(transitionTime);
+        
+        SceneManager.LoadScene("GenerateDungeonScene");
+    }
+    
+    
+}
--- WS_RLT/Assets/Script/Menus/MainMenu.cs
+++ WS_RLT/Assets/Script/Menus/MainMenu.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
+    public LevelLoader levelLoader;
+    private GameObject Script;
+    
+
     public void PlayGame()
     {
         Debug.Log("Loaded Scene 1 Index");
-        SceneManager.LoadScene(0);
+        levelLoader.Transition();
     }
 
     public void QuitGame()
