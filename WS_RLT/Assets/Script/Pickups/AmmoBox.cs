3de9d50e14cbc614a1329a319ebd81aea5c244ae
Lumiinaty <romain.nicoghossian@gmail.com>
2026-02-17 12:08:46 +0100

    TECH - Added meshes for pickups, added ammo pickup working, EDITED FIRE.CS

---
+++ WS_RLT/Assets/Script/Pickups/AmmoBox.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+
+    private Collider _collider;
+    private GameObject _player;
+    private Fire _ammo;
+    public int _ammoToGive;
+
+
+    public void Start()
+    {
+        _collider = GetComponent<SphereCollider>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _ammo = _player.GetComponent<Fire>();
+            _ammo.AddAmmo(_ammoToGive);
+            
+            Destroy(this.gameObject);
+        }
+    }
+    
+    
+}
